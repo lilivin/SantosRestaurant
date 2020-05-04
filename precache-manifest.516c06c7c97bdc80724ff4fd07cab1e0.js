80d32a6cad65122e0c57eeec2416b50c0b25c852
KLiwin <k.liwinski@gmail.com>
2020-05-05 00:07:18 +0200

    Updates

---
+++ precache-manifest.516c06c7c97bdc80724ff4fd07cab1e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c7dfbbfbdf3a898c80b8c8d1a163ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3053ac843454db8c1a7c",
+    "url": "/static/css/main.c9b812ad.chunk.css"
+  },
+  {
+    "revision": "f51176ac89cbfc8c6a0f",
+    "url": "/static/js/2.63d6bee4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.63d6bee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3053ac843454db8c1a7c",
+    "url": "/static/js/main.9456bd2b.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "dcdfe0b6ec67a11b5569a7fdfc8ffcfc",
+    "url": "/static/media/outBar.dcdfe0b6.jpg"
+  },
+  {
+    "revision": "f7967a5d5eef1b8d59a5578599f3a68e",
+    "url": "/static/media/outSide.f7967a5d.jpg"
+  },
+  {
+    "revision": "e3965358bdbc193424a0258bd11b602a",
+    "url": "/static/media/restaurant.e3965358.jpg"
+  }
+]);
